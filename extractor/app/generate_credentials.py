07b8a96be09e0429d17195d634c0add229439bcd
João Pedro <joaopedrodasilvalima@gmail.com>
2024-07-22 17:21:38 -0300

    FIX: Generate credentials

--- extractor/app/generate_credentials.py
+++ extractor/app/generate_credentials.py
@@ -11,31 +11,32 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+TOKEN_FILE       = "token.json"
+CREDENTIALS_FILE = "credentials.json"
 
+def generate_credentials():
+	"""
+		Generate a new authorization token to download e-mail files. 
+	"""
+	creds = None
+	# The file token.json stores the user's access and refresh tokens, and is
+	# created automatically when the authorization flow completes for the first
+	# time.
+	if os.path.exists(TOKEN_FILE):
+		creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
 
-def main():
-  """Shows basic usage of the Gmail API.
-  Lists the user's Gmail labels.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+	# If there are no (valid) credentials available, let the user log in.
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file(
+			CREDENTIALS_FILE, SCOPES
+			)
+			creds = flow.run_local_server(port=0)
+	# Save the credentials for the next run
+	with open(TOKEN_FILE, "w") as token:
+		token.write(creds.to_json())
 
 if __name__ == "__main__":
-  main()
+	generate_credentials()
