90bce73ace6acc4a9ad4d80edc86137dca137880
João Pedro <joaopedrodasilvalima@gmail.com>
2024-07-22 19:43:48 -0300

    DOC: EmailFileDownloader Class

--- extractor/app/download_files.py
+++ extractor/app/download_files.py
@@ -1,17 +1,17 @@
 import os
-import base64
-import datetime
-import io
-import logging
 import json
+import logging
+import datetime
+import base64
+import io
 
 from tests import test_if_token_is_still_active, test_token_exists
 
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+from typing import List, Tuple, Optional, Dict, Any
 
 # Configuração do logger
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -21,54 +21,73 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 # [WIP] Tudo isso vai virar variável de ambiente
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 TOKEN_PATH = 'token.json'
-lab_emails = ""
 
 class EmailFileDownloader:
+    """
+    Class used to download the data sent by email by the labs.
+
+    Uses Google API to authenticate and get access to the inbox.
+    """
 
     def __init__(self) -> None:
-        self.email_list_file = 'emails.json' # json file containing a list of each labs' emails
-        self.token_path      = 'token.json'  # json file containing the authorization token to the google API
-        self.scopes          = ["https://www.googleapis.com/auth/gmail.readonly"]
+        self.email_list_file = 'emails.json'  # JSON file containing a list of each lab's emails
+        self.token_path = 'token.json'  # JSON file containing the authorization token to the Google API
+        self.scopes = ["https://www.googleapis.com/auth/gmail.readonly"]
         self.last_download_time_file = 'last_download_time.txt'
 
         self.logger = logging.getLogger("FILE DOWNLOADER")
 
-
     def run(self) -> None:
-        email_list    = self.load_email_list_from_file()
+        """
+        Main method to run the email file downloader process.
+        """
+        email_list = self.load_email_list_from_file()
         gmail_service = self.get_google_api_service()
         self.download_attachments_from_email(email_list, gmail_service)
-
         self.write_last_email_download_time()
 
+    def load_email_list_from_file(self) -> Optional[Dict[str, List[str]]]:
+        """
+        Loads the email list from the specified JSON file.
 
-    def load_email_list_from_file(self):
+        Returns:
+            dict: A dictionary with lab names as keys and lists of emails as values.
+        """
         if not os.path.exists(self.email_list_file):
             self.logger.error(f"Unable to load file {self.email_list_file}.")
             return None
-        
+
         with open(self.email_list_file, 'r') as arquivo:
             email_list = json.load(arquivo)
 
         self.logger.info(f"Successfully loaded {self.email_list_file}")
-
         return email_list
-    
 
-    def get_google_api_service(self):
+    def get_google_api_service(self) -> Optional[Any]:
+        """
+        Authenticates with the Google API and returns the Gmail service object.
+
+        Returns:
+            googleapiclient.discovery.Resource: The Gmail service object.
+        """
         creds = Credentials.from_authorized_user_file(self.token_path, self.scopes)
         try:
-            # Call the Gmail API
             service = build('gmail', 'v1', credentials=creds)
         except HttpError as error:
             self.logger.error(f"An error occurred: {error}")
+            return None
 
         self.logger.info("Successfully authenticated in the Google API")
         return service
-    
 
-    def download_attachments_from_email(self, email_list, gmail_service):
+    def download_attachments_from_email(self, email_list: Dict[str, List[str]], gmail_service: Any) -> None:
+        """
+        Downloads attachments from emails based on the given email list and Gmail service.
 
+        Args:
+            email_list (dict): A dictionary with lab names as keys and lists of emails as values.
+            gmail_service (googleapiclient.discovery.Resource): The Gmail service object.
+        """
         labs = list(email_list.keys())
         self.logger.info(f"Downloading files from the following labs: {', '.join(labs)}")
 
@@ -76,7 +95,7 @@ class EmailFileDownloader:
         past_date = current_date - datetime.timedelta(days=1)
         past_date_str = past_date.strftime('%Y/%m/%d')
 
-        self.logger.info(f"Considering only e-mails after {past_date.strftime('%Y-%m-%d')}")
+        self.logger.info(f"Considering only emails after {past_date.strftime('%Y-%m-%d')}")
 
         for lab, emails in email_list.items():
             self.logger.info(f"Downloading files for {lab}")
@@ -90,8 +109,17 @@ class EmailFileDownloader:
                 lab_attachment_buffers = self.download_attachments_from_lab(email_query, gmail_service)
                 self.save_attachments_in_folder(lab_attachment_buffers, lab, lab_data_path)
 
+    def download_attachments_from_lab(self, email_query: str, gmail_service: Any) -> List[Tuple[str, io.BytesIO, str, str]]:
+        """
+        Downloads attachments from the lab emails based on the given query.
 
-    def download_attachments_from_lab(self, email_query, gmail_service):
+        Args:
+            email_query (str): The query to search for emails.
+            gmail_service (googleapiclient.discovery.Resource): The Gmail service object.
+
+        Returns:
+            list: A list of tuples containing the filename, buffer, date, and sender of each attachment.
+        """
         attachment_buffers = []
 
         results = gmail_service.users().messages().list(userId='me', q=email_query).execute()
@@ -102,7 +130,6 @@ class EmailFileDownloader:
             return attachment_buffers
 
         for message in messages:
-            
             msg = gmail_service.users().messages().get(userId='me', id=message['id']).execute()
 
             headers = msg['payload'].get('headers', [])
@@ -124,50 +151,54 @@ class EmailFileDownloader:
 
         return attachment_buffers
 
+    def save_attachments_in_folder(self, attachments: List[Tuple[str, io.BytesIO, str, str]], lab_name: str, path: str) -> None:
+        """
+        Saves the downloaded attachments to the specified folder.
 
-    def save_attachments_in_folder(self, attachments, lab_name, path):
-        # Create the directory if it doesn't exist
+        Args:
+            attachments (list): A list of tuples containing the filename, buffer, date, and sender of each attachment.
+            lab_name (str): The name of the lab.
+            path (str): The path to save the attachments.
+        """
         if not os.path.exists(path):
-            self.logger.error( f"Unable to salve files from `{lab_name}` into folder - {path} does not exist" )
+            self.logger.error(f"Unable to save files from `{lab_name}` into folder - {path} does not exist")
             return
 
         for filename, buffer, date, sender in attachments:
-            # Format date to be used in the filename
-            # Date Format in GMAIL -> YYYY-MM-DD
             date_str = datetime.datetime.strptime(date, '%a, %d %b %Y %H:%M:%S %z').strftime('%Y-%m-%d')
-            # Create the new filename
             new_filename = f"{lab_name}_{date_str}__{filename}"
-            # Define the path to save the file
             file_path = os.path.join(path, new_filename)
-            # Save the file
             with open(file_path, 'wb') as f:
                 f.write(buffer.getvalue())
 
             self.logger.info(f"Saved attachment {filename} as {new_filename}")
-        
 
+    def get_last_email_download_time(self) -> datetime.datetime:
+        """
+        Gets the last email download time from the specified file.
 
-    def get_last_email_download_time(self):
+        Returns:
+            datetime.datetime: The datetime object representing the last email download time.
+        """
         TIME_FORMAT = "%Y-%m-%d"
 
         if not os.path.exists(self.last_download_time_file):
             self.write_last_email_download_time()
 
-        last_email_download_time = None
         with open(self.last_download_time_file, 'r') as f:
-            last_email_download_time = datetime.datetime.strptime(
-                f.read(), TIME_FORMAT
-            )
+            last_email_download_time = datetime.datetime.strptime(f.read(), TIME_FORMAT)
 
         return last_email_download_time
-    
 
-    def write_last_email_download_time(self):
+    def write_last_email_download_time(self) -> None:
+        """
+        Writes the current date and time to the specified file as the last email download time.
+        """
         TIME_FORMAT = "%Y-%m-%d"
 
         current_date = datetime.datetime.now()
         with open(self.last_download_time_file, 'w') as f:
-            f.write( current_date.strftime(TIME_FORMAT) )
+            f.write(current_date.strftime(TIME_FORMAT))
 
 
 if __name__ == "__main__":
