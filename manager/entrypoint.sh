1b4895ca78f0d0066813275275fdfaf785b53965
João Pedro <joaopedrodasilvalima@gmail.com>
2024-10-24 18:52:07 -0300

    FEAT: Check connection to MinIO

---
+++ manager/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run health checks
+python /app/healthcheck/check.py
+
+# Check if the health check passed
+if [ $? -eq 0 ]; then
+  echo "Health checks passed. Starting Streamlit..."
+
+  # Start API
+  uvicorn main:app --reload --host 0.0.0.0 --port 8000
+else
+  echo "Health checks failed. Exiting."
+  exit 1
+fi
