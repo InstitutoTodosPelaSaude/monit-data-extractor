5a0e1064190d3afeb8eec1bdc9c53de6c4c470c0
João Pedro <joaopedrodasilvalima@gmail.com>
2024-10-25 02:15:17 -0300

    FEAT: Create session and save its STATUS

--- manager/app/main.py
+++ manager/app/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from datetime import datetime
 import random
+from sqlalchemy.orm import Session
 
+from models import get_db, Status
 
 app = FastAPI()
 
@@ -10,5 +12,12 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/log")
-async def root( app_name:str ):
-    return f"{app_name}-{datetime.now().strftime('%Y%m%d%H%m%S')}-{random.randint(0, 1000000):07d}"
+async def root( app_name:str, db: Session = Depends(get_db) ):
+
+    session_id = f"{app_name}-{datetime.now().strftime('%Y%m%d%H%m%S')}-{random.randint(0, 1000000):07d}"
+    new_status = Status(session_id=session_id, app_name=app_name, status="STARTED")
+    db.add(new_status)
+    db.commit()
+    db.refresh(new_status)
+
+    return session_id
--- manager/app/models.py
+++ manager/app/models.py
@@ -1,6 +1,6 @@
 
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.orm import sessionmaker, Session, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from datetime import datetime
 
 from enum import Enum
@@ -8,12 +8,14 @@ from enum import Enum
 # Configuração do SQLite
 DATABASE_URL = "sqlite:////data/monitor.db"
 Base = declarative_base()
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Modelo para a tabela Status
 class Status(Base):
     __tablename__ = "status"
 
-    session_id = Column(Integer, primary_key=True, index=True)
+    session_id = Column(String, primary_key=True, index=True)
     app_name = Column(String)
     start = Column(DateTime, default=datetime.now())
     end = Column(DateTime, nullable=True)
@@ -24,7 +26,7 @@ class Log(Base):
     __tablename__ = "log"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
 
-    session_id = Column(Integer, index=True)
+    session_id = Column(String, index=True)
     app_name   = Column(String)
     type       = Column(String)
     message    = Column(String)
@@ -35,15 +37,20 @@ class File(Base):
     __tablename__ = "file"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
 
-    session_id   = Column(Integer,)
+    session_id   = Column(String,)
     organization = Column(String, )
     project      = Column(String, )
     filename     = Column(String, )
     upload_ts    = Column(DateTime, default=datetime.now())
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 if __name__ == "__main__":
-    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
     Base.metadata.create_all(bind=engine)
