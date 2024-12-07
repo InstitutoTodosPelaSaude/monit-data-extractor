9870517593be9f960d4e531cf2fc4b01c4d41519
João Pedro <joaopedrodasilvalima@gmail.com>
2024-12-07 16:19:38 -0300

    FEAT: Heathchecks for gmail extractor

--- gmail-extractor/app/healthcheck/check.py
+++ gmail-extractor/app/healthcheck/check.py
@@ -1,11 +1,9 @@
 import os
 import sys
+import json
 
 import requests
 from http import HTTPStatus
-
-# TODO: Check for GMAIL env variables
-# TODO: Check if the file emails.json is defined
 
 def manager_api_is_reachable():
     # Get environment variables
@@ -21,12 +19,46 @@ def manager_api_is_reachable():
             return False, "Manager endpoint is not reachable"
     except Exception as e:
         return False, f"Exception during request {e}"
+    
+def gmail_env_variables_are_set():
+    gmail_env_variables = [
+        "EMAIL_ADDRESS",
+        "EMAIL_APP_PASSWORD"
+    ]
 
+    missing_vars = []
 
+    for gmail_env_var in gmail_env_variables:
+        if not os.getenv(gmail_env_var):
+            missing_vars.append(gmail_env_var)
+
+    if missing_vars:
+        return False, f"The following env variables are not set in .env: {','.join(missing_vars)}"
+    return True, "Able to read GMail env variables"
+    
+def emails_json_is_correctly_defined():
+    emails_json_path = '/app/emails.json'
+    
+    # Verifica se o arquivo existe
+    if not os.path.exists(emails_json_path):
+        return False, f"File {emails_json_path} does not exist. Create a file emails.json using the emails-example.json as a model."
+    
+    try:
+        # Verifica se o conteúdo do arquivo é um JSON válido
+        with open(emails_json_path, 'r') as file:
+            json.load(file)
+        return True, "emails.json is correctly defined."
+    except json.JSONDecodeError as e:
+        return False, f"Invalid JSON format in {emails_json_path}: {e}"
+    except Exception as e:
+        return False, f"Error while reading {emails_json_path}: {e}"
+    
 def healthcheck():
     # Run multiple health checks
     checks = {
-        "Manager API is Reachable": manager_api_is_reachable    
+        "Manager API is Reachable": manager_api_is_reachable,
+        "GMail env variables": gmail_env_variables_are_set,
+        "emails.json is defined": emails_json_is_correctly_defined
     }
 
     all_healthy = True
