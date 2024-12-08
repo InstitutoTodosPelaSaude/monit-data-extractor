ed563fd2a6f37712ce8dc6a67b8e2094eef1edc6
João Pedro <joaopedrodasilvalima@gmail.com>
2024-12-08 16:26:42 -0300

    FEAT: Notifier send messages periodically to slack informing about lab's files

--- manager/app/notifier/notifier.py
+++ manager/app/notifier/notifier.py
@@ -1,15 +1,21 @@
 import datetime
 import sqlite3
 
+import json
+import requests
+from http import HTTPStatus
+
 DB_PATH = '/data/monitor.db'
 MINIMUM_TIME_FOR_NOTIFICATION_IN_DAYS = 5
+MANAGER_API_URL = "http://manager:8000"
 
 # Convert the minimum notification time into a timedelta object
 time_limit = datetime.timedelta(days=MINIMUM_TIME_FOR_NOTIFICATION_IN_DAYS)
 
 labs_monitored = ['fleury', 'einstein',
                   'sabin' , 'hlagyn',
-                  'hilab' , 'hpardini']
+                  'hilab' , 'hpardini',
+                  'dbmol', ]
 
 projects = ['arbo', 'respat']
 
@@ -28,9 +34,7 @@ query_latest_file_for_each_organization = f"""
     WHERE 
         DATE(upload_ts) >= last_friday.friday_date
     AND organization 
-    IN ('fleury', 'einstein',
-        'sabin' , 'hlagyn',
-        'hilab' , 'hpardini')
+    IN ('{"','".join(labs_monitored)}')
     ;
 """
 
@@ -38,6 +42,7 @@ query_latest_file_for_each_organization = f"""
 def create_summary():
     """
     Generates a summary of the file uploads for each project and organization.
+    Built with help of https://app.slack.com/block-kit-builder.
     
     Returns:
         str: A formatted string containing the summary report.
@@ -54,28 +59,71 @@ def create_summary():
         results = cursor.fetchall()
         
         # Process results
-        summary = {project: {lab: {"status": "🔴", "files": []} for lab in labs_monitored} for project in projects}
+        summary = {
+            project: {lab: {"status": "🔴", "files": []} for lab in labs_monitored} 
+            for project in projects
+        }
         
         # Fill the summary based on query results
         for project, organization, filename, upload_ts in results:
-            upload_date = datetime.datetime.fromisoformat(upload_ts)
             summary[project][organization]["status"] = "🟢"
-            summary[project][organization]["files"].append((filename, upload_date.strftime("%Y-%m-%d")))
+            summary[project][organization]["files"].append(filename.split('__')[-1])
 
-        # Generate the report
+        # Generate the Slack report
+        slack_summary_report = {
+            'blocks':[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Arquivos enviados pelos laboratórios - Resumo Diário"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+            ]
+        }
+
         for project, organizations in summary.items():
-            summary_report.append(f"\n--- {project.upper()} ---")
+            slack_summary_report['blocks'].append(
+                {'type': 'header', 'text':{'type': 'plain_text', 'text': project.capitalize()}}
+            )
+            
             for organization, info in organizations.items():
-                summary_report.append(f"{organization}: {info['status']}")
-                if info["files"]:
-                    for filename, date in info["files"]:
-                        summary_report.append(f"  - {filename} (uploaded on {date})")
-                else:
-                    summary_report.append("  No files found.")
-    
-    # Join the summary lines into a single string
-    return "\n".join(summary_report)
+                files = '\n'.join(info['files'])
+                slack_summary_report['blocks'].append(
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f"{info['status']} *{organization.upper()}* \n {files}"
+                        }
+                    }
+                )
 
-# Run the summary function and store the result
-report = create_summary()
-print(report)
+            slack_summary_report['blocks'].append(
+                {'type': 'divider'}
+            )  
+    
+    return slack_summary_report
+
+
+if __name__ == "__main__":
+
+    slack_summary = create_summary()
+    slack_summary_json = json.dumps(slack_summary)
+    slack_summary_json = str(slack_summary_json)
+
+    try:
+        response = requests.post(
+            F"{MANAGER_API_URL}/notify/slack",
+            params={
+                "message": slack_summary_json
+            }
+        )
+        response.raise_for_status()
+        print(f"File summary successfully sent.")
+    except Exception as e:
+        print(f"Error sending daily summary. {e}")
+        print(slack_summary_json)
