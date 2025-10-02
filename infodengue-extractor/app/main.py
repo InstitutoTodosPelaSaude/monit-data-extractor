0ae0f50489c2ecca895e7e10f3b8e0b301dfc336
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2025-10-02 16:34:22 -0300

    FIX: fix ibge file path

--- infodengue-extractor/app/main.py
+++ infodengue-extractor/app/main.py
@@ -12,7 +12,7 @@ from itertools import product
 from log import ManagerInterface
 
 # Prepare city and state information from ibge file
-ibge_df = pd.read_csv("data/ibge_municipios_full.csv", dtype=str)
+ibge_df = pd.read_csv("ibge_municipios_full.csv", dtype=str)
 
 cities_to_extract = dict()
 for index, row in ibge_df.iterrows():
